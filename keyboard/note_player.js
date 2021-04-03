2a33e39e453dd1141ecc322f580549d0f56549be
agctu <agctu46@outlook.com>
2021-04-03 16:28:19 +0800

    Add note_player.js
    This is a file using webaudio to control note playing.

---
+++ keyboard/note_player.js
@@ -0,0 +1,29 @@
+//play multiple notes simultaneously, but every note in playing should have a different value.
+class NotePlayer{
+    constructor(maptable){
+        this.mapping=maptable;
+        this.note_playing=[];
+        this.audio_ctx=new AudioContext();
+        this.amp=this.audio_ctx.createGain();
+        this.amp.gain.value=1;
+    }
+    play(noteId){
+        if(this.note_playing[noteId])return;
+        var osc=this.audio_ctx.createOscillator();
+        osc.frequency.value=this.mapping.getFrequencyById(noteId);
+        this.note_playing[noteId]=osc;
+        osc.connect(this.amp).connect(this.audio_ctx.destination);
+        osc.start();
+    }
+    end(noteId){
+        if(!this.note_playing[noteId])return;
+        var osc=this.note_playing[noteId];
+        osc.stop();
+    }
+    set volume(value){
+        this.amp.gain.value=value;
+    }
+    get volume(){
+        return this.amp.gain.value;
+    }
+}
