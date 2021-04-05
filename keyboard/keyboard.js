6c3391c3b1975ae651f548ced50b2831c13388c3
agctu <agctu46@outlook.com>
2021-04-05 18:35:06 +0800

    Add key map codes

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -5,8 +5,25 @@ class Keyboard{
         this.plotter=plotter;
         this.nKeys;// should set one default here
         this.key_offset=40;// should change the value later 
+        this.key_map={};
+        for(let i=0;i<Keyboard.keys.length;++i){
+            for(let j=0;j<Keyboard.keys[i].length;++j){
+                this.key_map[Keyboard.keys[i][j]]=[i,j];
+            }
+        }
     }
-    
+    findKeyId(key){
+        var key_pos=this.key_map[key];
+        return (4+key_pos[0])*(12)+key_pos[1];
+    }
+    keyDown(key){
+        var key_id=this.findKeyId(key);
+        this.player.play(key_id);
+    }
+    keyUp(key){
+        var key_id=this.findKeyId(key);
+        this.player.stop(key_id);
+    }
 }
 Keyboard.keys=[
     ['1','2','3','4','5','6','7','8'],
@@ -31,7 +48,7 @@ class Sequence{
             this.len=data["len"];
             this.notes=data["notes"];
         }
-        else if(seq_str.constructor==){//parse from MIDI binary
+        else if(seq_str.constructor==Array){//parse from MIDI binary
             throw "Not Implemented!";
         }
         else{
