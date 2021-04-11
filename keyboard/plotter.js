714f5ccdd73e35cb21ae7fca97469db5b1349ea4
agctu <agctu46@outlook.com>
2021-04-11 23:29:51 +0800

    Reconstruct and add more
    Use this.whiteKeyWidth and this.blackKeyWidth to store the value
    of the width of keys,add a getKeyLeftPosition method, which
    is called from only a few places,and add more code for drawing
    note. Now a note is generated when key is pressed down, but it
    neither move or disappear,which will be implemented later.

--- keyboard/plotter.js
+++ keyboard/plotter.js
@@ -5,13 +5,30 @@ function isWhiteKey(keyId){
 }
 //draw keys and notes
 class Plotter{
+    blackKeyOffset=[0.5,1.8,3.5,4.7,5.8];
     constructor(nWhiteKey,offset,noteDisplayTime){
         this.width=window.innerWidth;
         this.height=window.innerHeight;
+        this.whiteKeyWidth=this.width/nWhiteKey;
+        this.blackKeyWidth=this.whiteKeyWidth*0.55;
         this.offset=offset;
         this.noteDisplayTime=noteDisplayTime;
         this.notes=[];
-        this.initKeys(nWhiteKey);
+        this.initKeys();
+    }
+    createNote(keyId){
+        var noteBlock=document.createElement("div");
+        noteBlock.style.position="absolute";
+        noteBlock.style.left=this.getKeyLeftPosition(keyId)+"px";
+        noteBlock.style.top=this.height/2+"px";
+        noteBlock.style.width=(isWhiteKey(keyId)?
+        this.whiteKeyWidth:
+        this.blackKeyWidth)+"px";
+        noteBlock.style.height=0+"px";
+        noteBlock.style.background="#99f";
+        noteBlock.style.borderRadius="5px 5px 5px 5px";
+        document.body.append(noteBlock);
+        return {state: "dynamic",id: keyId,block: noteBlock};
     }
     //keyDown and keyUp modify the state of the key and add note above keys
     keyDown(keyId){
@@ -27,18 +44,6 @@ class Plotter{
         var newNote=this.createNote(keyId);
         this.notes.push(newNote);
     }
-    createNote(keyId){
-        var noteBlock=document.createElement("div");
-        noteBlock.position="absolute";
-        noteBlock.style.left=;
-        noteBlock.style.top=this.width/2;
-        noteBlock.style.width=isWhiteKey(keyId)?
-        this.width/this.nWhiteKey:
-        this.width/this.nWhiteKey*0.55;
-        noteBlock.style.height=0;
-        noteBlock.style.background="blue";
-        return {state: "pressed",block: noteBlock};
-    }
     keyUp(keyId){
         if(keyId<0||keyId>=this.keys.length)return;
         var tarKey=this.keys[keyId];
@@ -49,6 +54,9 @@ class Plotter{
             tarKey.style.background="black";
         }
         //modify the state of the note
+        for(let i=0;i<this.notes.length;++i)if(this.notes[i].state=="dynamic"&&this.notes[i].id==keyId){
+            this.notes[i].state="static";
+        }
     }
 
     draw(){
@@ -63,7 +71,7 @@ class Plotter{
         //FIXME how to stop this, because every anonymous function is different.
         requestAnimationFrame(function(){tar.startDraw();});
     }
-    initKeys(nWhiteKey){
+    initKeys(){
         var totalN=128
         this.keys=[];
         //add all keys no matter it is visible
@@ -79,7 +87,7 @@ class Plotter{
         //for each key, set its style according to its color
         for(let i=0;i<totalN;++i){
             if(isWhiteKey(i)){
-                this.keys[i].style.width=this.width/nWhiteKey+"px";
+                this.keys[i].style.width=this.whiteKeyWidth+"px";
                 this.keys[i].style.height=this.height/2+"px";
                 this.keys[i].style.background="#ffffff";
                 this.keys[i].style.borderRadius="0px 0px 4px 4px";
@@ -87,7 +95,7 @@ class Plotter{
                 wtb.firstElementChild.append(this.keys[i]);
             }
             else{//key is black
-                this.keys[i].style.width=this.width/nWhiteKey*0.55+"px";
+                this.keys[i].style.width=this.blackKeyWidth+"px";
                 this.keys[i].style.height=this.height/4+"px";
                 this.keys[i].style.background="#000000";
                 this.keys[i].style.borderRadius="0px 0px 4px 4px";
@@ -98,19 +106,31 @@ class Plotter{
         wtb.style.position="absolute";
         wtb.style.left=0+"px";
         wtb.style.top=this.height/2+"px";
-        wtb.style.width=this.width/nWhiteKey*75+"px";
+        wtb.style.width=this.whiteKeyWidth*75+"px";
         document.body.append(wtb);
-        //document.body.scrollTo(this.width/nWhiteKey*this.offset,0);
+        //document.body.scrollTo(this.whiteKeyWidth*this.offset,0);
 
         //don't put black keys in a table
-        var blackKeyOffset=[0.5,1.8,3.5,4.7,5.8];
         for(let i=0;i<this.bkeys.length;++i){
             this.bkeys[i].style.position="absolute";
             this.bkeys[i].style.left=
-            Number.parseInt(i/5)*this.width/nWhiteKey*7
-            +blackKeyOffset[i%5]*this.width/nWhiteKey+"px";
+            Number.parseInt(i/5)*this.whiteKeyWidth*7+
+            this.blackKeyOffset[i%5]*this.whiteKeyWidth+"px";
             this.bkeys[i].style.top=this.height/2+"px";
             document.body.append(this.bkeys[i]);
         }
+    }
+    getKeyLeftPosition(keyId){
+        return isWhiteKey(keyId)?
+            this.whiteKeyWidth*(
+                Number.parseInt(keyId/12)*7+
+                ((keyId%12+(keyId%12>4))>>>1)
+            ):
+            this.whiteKeyWidth*(
+                Number.parseInt(keyId/12)*7+
+                this.blackKeyOffset[
+                    ((keyId%12-(keyId%12>3))>>>1)
+                ]
+            );
     }
 }
